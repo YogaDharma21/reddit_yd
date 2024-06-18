d9881fedf2c3a20760a328696110f540233b8b33
YogaDharma21 <yogacode2006@gmail.com>
2024-06-18 12:32:43 +0800

    feat: add success and error toast notifications for username edit

--- components/SettingsForm.tsx
+++ components/SettingsForm.tsx
@@ -7,14 +7,36 @@ import { Label } from "./ui/label";
 import { Separator } from "./ui/separator";
 import { updateUsername } from "@/app/action";
 import SubmitButton from "./SubmitButton";
-
+import { useFormState } from "react-dom";
+import { useEffect } from "react";
+import { useToast } from "./ui/use-toast";
+const initialState = {
+  message: "",
+  status: "",
+};
 export function SettingsForm({
   username,
 }: {
   username: string | null | undefined;
 }) {
+  const [state, formAction] = useFormState(updateUsername, initialState);
+  const { toast } = useToast();
+  useEffect(() => {
+    if (state?.status === "green") {
+      toast({
+        title: "Succesfull",
+        description: state.message,
+      });
+    } else if (state?.status === "error") {
+      toast({
+        title: "Error",
+        description: state.message,
+        variant: "destructive",
+      });
+    }
+  }, [state, toast]);
   return (
-    <form action={updateUsername}>
+    <form action={formAction}>
       <h1 className="text-3xl font-extrabold tracking-tight">Settings</h1>
       <Separator className="my-4" />
       <Label className="text-lg">Username</Label>
@@ -29,6 +51,9 @@ export function SettingsForm({
         min={2}
         maxLength={21}
       />
+      {state?.status === "error" && (
+        <p className="text-red-500 mt-1">{state.message}</p>
+      )}
       <div className="w-full flex  mt-5 gap-x-5 justify-end">
         <Button variant={"secondary"} asChild type="button">
           <Link href={"/"}>Cancel</Link>
